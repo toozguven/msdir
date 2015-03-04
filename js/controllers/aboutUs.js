c1f8e4796da48a6c65a63cc01800edf92dea8816
toozguven <toozguven@gmail.com>
2015-03-04 14:32:32 +0000

    final1 4Mar2015 1432
    lean, inc appInfo btn on home and back btn on related pages

--- js/controllers/aboutUs.js
+++
@@ -1,7 +0,0 @@
-﻿ngapp.controller( 'AboutUsCtrl', ['$scope', 'factory', 'dataMgr', '$anchorScroll', '$location', '$timeout', function ( $scope, factory, dataMgr, $anchorScroll, $location, $timeout )
-{
-  $scope.helpers = factory.getHelpers();
-  $scope.helpers.showLoading = false;
-
-  $anchorScroll();
-}] );
